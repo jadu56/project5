285aa0178d90547aea9d2349def534ac14b303f9
Jadu Hembram <107939386+jadu56@users.noreply.github.com>
2025-08-24 15:36:18 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,16 @@
+numbers = list(range(1, 11))
+
+print("origianal numbers: ", numbers)
+
+first_five = numbers[:5]
+
+print("Extracted five numbers list:", first_five)
+
+numbers.reverse()
+
+print("Reversed list:", numbers)
+
+
+
+
+
